4d6026717630b584039462250d942e1d9a2188de
kazpc1215 <isoya.kazuhide@d.mbox.nagoya-u.ac.jp>
2017-07-07 15:08:54 +0900

    no message

--- Frag_test_equidistant/data/TotalMass.py
+++ Frag_test_equidistant/data/TotalMass.py
@@ -8,7 +8,7 @@ import numpy as np
 plt.figure(figsize=(10,8),dpi=100)
 
 ##############################################################################
-filename = "./deltar_0010au_0990_xi0005_cutting_edge_2/TotalMass.dat"
+filename = "./deltar_0010au_0990_xi001_05pi_cutting_edge_2/TotalMass.dat"
 
 GYOU = 65
 
@@ -25,17 +25,17 @@ M_tot_0[:] = arr[:,2]
 M_rel = M_tot/M_tot_0
 
 y=1.0/(1.0+time/10.0)
-plt.plot(time, y, color="black", label='analytic',lw=1)  #解析解
+#plt.plot(time, y, color="black", label='analytic',lw=1)  #解析解
 
-plt.scatter(time, M_rel, color="r", marker="x", label=r"$\xi=0.005$")  #data
+#plt.scatter(time, M_rel, color="r", marker="x", label=r"$\xi=0.01,\delta \theta=0.5 \pi$")  #data
 
 relative_error=np.absolute(M_rel-y)/y
-#plt.plot(time, relative_error , color="r", marker="x", label=r"$\xi=0.005$",lw=1)  #relative error
+plt.plot(time, relative_error , color="r", marker="x", label=r"$\xi=0.01,\delta \theta=0.5 \pi$",lw=1)  #relative error
 ##############################################################################
 
 
 ##############################################################################
-filename = "./deltar_0010au_0990_xi001_cutting_edge_2/TotalMass.dat"
+filename = "./deltar_0010au_0990_xi001_025pi_cutting_edge_2/TotalMass.dat"
 
 GYOU = 65
 
@@ -53,10 +53,10 @@ M_rel = M_tot/M_tot_0
 
 y=1.0/(1.0+time/10.0)
 
-plt.scatter(time, M_rel, color="b", marker="+", label=r"$\xi=0.01$")  #data
+#plt.scatter(time, M_rel, color="b", marker="+", label=r"$\xi=0.01, \delta \theta = 0.25 \pi$")  #data
 
 relative_error=np.absolute(M_rel-y)/y
-#plt.plot(time, relative_error, color="b", marker="+", label=r"$\xi=0.01$",lw=1)  #relative error
+plt.plot(time, relative_error, color="b", marker="+", label=r"$\xi=0.01, \delta \theta = 0.25 \pi$",lw=1)  #relative error
 ##############################################################################
 
 
@@ -71,8 +71,8 @@ plt.autoscale(enable=True, axis='y')
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 plt.xlabel('time[yr]',fontsize=25)
-plt.ylabel(r'$M_{\rm tot}/M_{\rm tot,0}$', fontsize=25)
-#plt.ylabel('relative error', fontsize=25)
+#plt.ylabel(r'$M_{\rm tot}/M_{\rm tot,0}$', fontsize=25)
+plt.ylabel('relative error', fontsize=25)
 plt.grid(True)
 plt.tight_layout()
 plt.legend(loc=1,fontsize=15)
