53fdfb03c381c0284cbc25caf6716a5ad83d8703
kazpc1215 <isoya.kazuhide@d.mbox.nagoya-u.ac.jp>
2017-09-11 18:28:41 +0900

    no message

--- Collision_Coalescence_3/source/orbital_elements.c
+++ Collision_Coalescence_3/source/orbital_elements.c
@@ -2,6 +2,24 @@
 
 /*軌道要素計算*/
 int Calculate_OrbitalElements(int i,double x_c[][4],double v_c[][4],struct orbital_elements ele[],double P[][4],double Q[][4],double r_c[],double v2_c[],double r_dot_v[]){
+
+#if INDIRECT_TERM
+  
+#if !defined(G) && !defined(M_0)
+  double mu = 1.0*(1.0 + ele[i].mass);
+#else
+  double mu = G*(M_0 + ele[i].mass);
+#endif
+
+#else
+  
+#if !defined(G) && !defined(M_0)
+  double mu = 1.0;
+#else
+  double mu = G*M_0;
+#endif
+
+#endif
   
   int k;
   double esin_u;
@@ -14,6 +32,7 @@ int Calculate_OrbitalElements(int i,double x_c[][4],double v_c[][4],struct orbit
   double cos_Omega;
   double radian;
 
+  
   if(isnan(x_c[i][1])||isnan(x_c[i][2])||isnan(x_c[i][3])){
     printf("i=%d\tx is nan.\t[1]=%f\t[2]=%f\t[3]=%f\n",i,x_c[i][1],x_c[i][2],x_c[i][3]);
   }
@@ -29,42 +48,38 @@ int Calculate_OrbitalElements(int i,double x_c[][4],double v_c[][4],struct orbit
   if(isnan(r_dot_v[i])){
     printf("i=%d\tr_dot_v is nan.\n",i);
   }
+  
 
-#if !defined(G) && !defined(M_0)
-  ele[i].axis = 1.0/(2.0/r_c[i] - v2_c[i]);
-#else
-  ele[i].axis = 1.0/(2.0/r_c[i] - v2_c[i]/(G*M_0));
-#endif
+  ele[i].axis = 1.0/(2.0/r_c[i] - v2_c[i]/mu);
+
   
   if(isnan(ele[i].axis)){
     printf("i=%d\taxis is nan.\n",i);
   }
 
+  
+  
   if(ele[i].axis<0.0){
     printf("i=%d\taxis=%e < 0 双曲線軌道. axis -> -1*axis\n",i,ele[i].axis);
     ele[i].axis *= -1.0;
     //printf("\tr=%f[AU]\tR_planet=%f\n",sqrt((x_c[i][1]-x_c[PLANET_NO][1])*(x_c[i][1]-x_c[PLANET_NO][1])+(x_c[i][2]-x_c[PLANET_NO][2])*(x_c[i][2]-x_c[PLANET_NO][2])+(x_c[i][3]-x_c[PLANET_NO][3])*(x_c[i][3]-x_c[PLANET_NO][3])),PLANET_RADIUS);
   }
 
-#if !defined(G) && !defined(M_0)
-  ele[i].ecc = sqrt((1.0-r_c[i]/ele[i].axis)*(1.0-r_c[i]/ele[i].axis) + r_dot_v[i]*r_dot_v[i]/ele[i].axis);
-#else
-  ele[i].ecc = sqrt((1.0-r_c[i]/ele[i].axis)*(1.0-r_c[i]/ele[i].axis) + r_dot_v[i]*r_dot_v[i]/(G*M_0*ele[i].axis));
-#endif
+
+  ele[i].ecc = sqrt((1.0-r_c[i]/ele[i].axis)*(1.0-r_c[i]/ele[i].axis) + r_dot_v[i]*r_dot_v[i]/mu/ele[i].axis);
+
   
   //printf("i=%d\tecc=%f\n",i,ele[i].ecc);
   if(isnan(ele[i].ecc)){
     printf("i=%d\tecc is nan.\n",i);
   }
   
+  
   if(ele[i].ecc==0.0){
     ele[i].u = 0.0;
   }else{
-#if !defined(G) && !defined(M_0)
-    esin_u = r_dot_v[i]/sqrt(ele[i].axis);
-#else
-    esin_u = r_dot_v[i]/sqrt(G*M_0*ele[i].axis);
-#endif
+    
+    esin_u = r_dot_v[i]/sqrt(mu*ele[i].axis);
     ecos_u = 1.0-r_c[i]/ele[i].axis;
     radian = atan2(esin_u,ecos_u);
     if(radian<0.0){
@@ -78,24 +93,12 @@ int Calculate_OrbitalElements(int i,double x_c[][4],double v_c[][4],struct orbit
     printf("i=%d\tu is nan.\n",i);
   }
 
-  
-  if(isnan(P[i][1])||isnan(P[i][2])||isnan(P[i][3])){
-    printf("i=%d\tpre P is nan.\t[1]=%f\t[2]=%f\t[3]=%f\n",i,P[i][1],P[i][2],P[i][3]);
-  }
-  if(isnan(Q[i][1])||isnan(Q[i][2])||isnan(Q[i][3])){
-    printf("i=%d\tpre Q is nan.\t[1]=%f\t[2]=%f\t[3]=%f\n",i,Q[i][1],Q[i][2],Q[i][3]);
-  }
 
   
   for(k=1;k<=3;++k){
     
-#if !defined(G) && !defined(M_0)
-    P[i][k] = x_c[i][k]*cos(ele[i].u)/r_c[i] - sqrt(ele[i].axis)*v_c[i][k]*sin(ele[i].u);
-    Q[i][k] = (x_c[i][k]*sin(ele[i].u)/r_c[i] + sqrt(ele[i].axis)*v_c[i][k]*(cos(ele[i].u)-ele[i].ecc))/sqrt(fabs(1.0-ele[i].ecc));
-#else
-    P[i][k] = x_c[i][k]*cos(ele[i].u)/r_c[i] - sqrt(ele[i].axis/(G*M_0))*v_c[i][k]*sin(ele[i].u);
-    Q[i][k] = (x_c[i][k]*sin(ele[i].u)/r_c[i] + sqrt(ele[i].axis/(G*M_0))*v_c[i][k]*(cos(ele[i].u)-ele[i].ecc))/sqrt(fabs(1.0-ele[i].ecc));
-#endif
+    P[i][k] = x_c[i][k]*cos(ele[i].u)/r_c[i] - sqrt(ele[i].axis/mu)*v_c[i][k]*sin(ele[i].u);
+    Q[i][k] = (x_c[i][k]*sin(ele[i].u)/r_c[i] + sqrt(ele[i].axis/mu)*v_c[i][k]*(cos(ele[i].u)-ele[i].ecc))/sqrt(1.0-ele[i].ecc);
   }
 
   if(isnan(P[i][1])||isnan(P[i][2])||isnan(P[i][3])){
@@ -187,6 +190,25 @@ double Calculate_Q(int i,int k,struct orbital_elements ele[]){
 
 /*初期位置、速度計算*/
 void InitialCondition(int i,double P[][4],double Q[][4],double x_0[][4],double v_0[][4],double v2_0[],double r_dot_v[],double r_0[],struct orbital_elements ele[]){
+
+#if INDIRECT_TERM
+  
+#if !defined(G) && !defined(M_0)
+  double mu = 1.0*(1.0 + ele[i].mass);
+#else
+  double mu = G*(M_0 + ele[i].mass);
+#endif
+
+#else
+  
+#if !defined(G) && !defined(M_0)
+  double mu = 1.0;
+#else
+  double mu = G*M_0;
+#endif
+
+#endif
+
   int k;
   for(k=1;k<=3;k++){
     P[i][k] = Calculate_P(i,k,ele);
@@ -202,11 +224,8 @@ void InitialCondition(int i,double P[][4],double Q[][4],double x_0[][4],double v
       
   for(k=1;k<=3;++k){
 
-#if !defined(G) && !defined(M_0)
-    v_0[i][k] = sqrt(1.0/ele[i].axis)/r_0[i]*(-ele[i].axis*P[i][k]*sin(ele[i].u) + ele[i].axis*sqrt(1.0-ele[i].ecc*ele[i].ecc)*Q[i][k]*cos(ele[i].u));
-#else
-    v_0[i][k] = sqrt(1.0/ele[i].axis)/r_0[i]*(-ele[i].axis*P[i][k]*sin(ele[i].u) + ele[i].axis*sqrt(1.0-ele[i].ecc*ele[i].ecc)*Q[i][k]*cos(ele[i].u));
-#endif
+    
+    v_0[i][k] = sqrt(mu/ele[i].axis)/r_0[i]*(-ele[i].axis*P[i][k]*sin(ele[i].u) + ele[i].axis*sqrt(1.0-ele[i].ecc*ele[i].ecc)*Q[i][k]*cos(ele[i].u));
   }
 
   r_dot_v[i] = InnerProduct(i,x_0,v_0);  //r_i,v_iの内積.
