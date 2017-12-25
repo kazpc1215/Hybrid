2aa7145573f2d57a4e684fee9b395c834cd03ac3
kazpc1215 <isoya.kazuhide@d.mbox.nagoya-u.ac.jp>
2017-12-25 16:44:29 +0900

    no message

--- Omp_test_2/source/func.h
+++ Omp_test_2/source/func.h
@@ -160,3 +160,25 @@ static inline int Max_int(int a, int b){
 static inline double Cal_time(struct timeval time1, struct timeval time2){
   return (double)(time2.tv_sec - time1.tv_sec) + (double)(time2.tv_usec - time1.tv_usec)*1.0E-6;
 }
+
+
+static inline void Predictor(int i,CONST double x_0[][4],CONST double v_0[][4],CONST double a_0[][4],CONST double adot_0[][4],double x_p[][4],double v_p[][4],double r_p[],double v2_p[],double r_dot_v[],CONST double Dt[]){
+
+  int k;
+  double dt = Dt[i];
+
+  for(k=1;k<=3;++k){
+    //x_p[i][k] = x_0[i][k] + v_0[i][k]*Dt[i] + a_0[i][k]*Dt[i]*Dt[i]/2.0 + adot_0[i][k]*Dt[i]*Dt[i]*Dt[i]/6.0;
+    //v_p[i][k] = v_0[i][k] + a_0[i][k]*Dt[i] + adot_0[i][k]*Dt[i]*Dt[i]/2.0;
+
+    x_p[i][k] = x_0[i][k] + dt*(v_0[i][k] + dt*0.5*(a_0[i][k] + adot_0[i][k]*dt*INV_3));
+    v_p[i][k] = v_0[i][k] + dt*(a_0[i][k] + adot_0[i][k]*dt*0.5);
+  }
+
+
+  r_p[i] = RadiusFromCenter(i,x_p);  //中心星からの距離.
+  v2_p[i] = SquareOfVelocity(i,v_p); //速度の2乗.
+  r_dot_v[i] = InnerProduct(i,x_p,v_p);  //r_i,v_iの内積.
+
+  return;
+}
