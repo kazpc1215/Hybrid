96597bc6cff2c97a44884ba1adc26d601cd09ed7
kazpc1215 <isoya.kazuhide@d.mbox.nagoya-u.ac.jp>
2017-12-29 03:00:49 +0900

    Makefileでjob.shを作る際に$の出力方法を変えた．

--- Dynamical_Friction/source/job.sh
+++ Dynamical_Friction/source/job.sh
@@ -5,4 +5,4 @@ cd $PBS_O_WORKDIR
 export OMP_NUM_THREADS=1
 export OMP_WAIT_POLICY=PASSIVE
 export CRAY_OMP_CHECK_AFFINITY=TRUE
-aprun -n 1 -d OMP_NUM_THREADS ./Ntr1E3_t2E0yr_dt1yr_Mtot3E-5_ecc1E-2_inc5E-3_rand1.exe >> /work/isoyakz/Dynamical_Friction/log/Ntr1E3_t2E0yr_dt1yr_Mtot3E-5_ecc1E-2_inc5E-3_rand1.txt
+aprun -n 1 -d $OMP_NUM_THREADS ./Ntr1E3_t2E0yr_dt1yr_Mtot3E-5_ecc1E-2_inc5E-3_rand1.exe >> /work/isoyakz/Dynamical_Friction/log/Ntr1E3_t2E0yr_dt1yr_Mtot3E-5_ecc1E-2_inc5E-3_rand1.txt
