d4e935828e210cfafa32eff85f6495f20e841b55
kazpc1215 <isoya.kazuhide@d.mbox.nagoya-u.ac.jp>
2018-06-22 23:22:01 +0900

    補完と履歴を使えるようにした．

--- Dynamical_Friction/source/qsub_depend.sh
+++ Dynamical_Friction/source/qsub_depend.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
 
+history -r .input_history
+
 while [ -z $SCRIPT ]
 do
-    read -r -p "Type the shell script name you want to run. (ex. *****.sh) : " SCRIPT
+    read -e -r -p "Type the shell script name you want to run. : " SCRIPT
 done
 echo "$SCRIPT"
+history -s "$SCRIPT"
 
 while [ -z $YESNO ]
 do
-    read -r -p "Is there a dependent job? (y / n) : " YESNO
+    read -e -r -p "Is there a dependent job? (y / n) : " YESNO
 done
 echo "$YESNO"
+history -s "$YESNO"
 
 if [ $YESNO = 'y' ] ; then
     
     while [ -z $JOBID ]
     do
-        read -r -p "Type a number to set the dependent job ID. (ex. *****.sdb) : " JOBID
+        read -e -r -p "Type a number to set the dependent job ID. (ex. *****.sdb) : " JOBID
     done
     echo "$JOBID"
+    history -s "$JOBID"
     
     while [ -z $NRUN ]
     do
-	read -r -p "Type a number to decide how many times to execute the job.: " NRUN
+	read -e -r -p "Type a number to decide how many times to execute the job.: " NRUN
     done
     echo "$NRUN"
-    
+    history -s "$NRUN"
+
     for i in `seq 1 $NRUN` ; do
-        qsub -W depend=afterok:$JOBID $SCRIPT.sh | tee tmp.txt
+        qsub -W depend=afterok:$JOBID $SCRIPT | tee tmp.txt
         JOBID=$(cat tmp.txt)
         echo ""
         echo "-----dependency of $JOBID-----"
@@ -44,15 +50,16 @@ elif [ $YESNO = 'n' ] ; then
 
     while [ -z $NRUN ]
     do
-        read -r -p "Type a number to decide how many times to execute the job.: " NRUN
+        read -e -r -p "Type a number to decide how many times to execute the job.: " NRUN
     done
     echo "$NRUN"
+    history -s "$NRUN"
     
     for i in `seq 1 $NRUN` ; do
 	if [ $i = 1 ] ; then
-	    qsub $SCRIPT.sh | tee tmp.txt
+	    qsub $SCRIPT | tee tmp.txt
 	else
-	    qsub -W depend=afterok:$JOBID $SCRIPT.sh | tee tmp.txt
+	    qsub -W depend=afterok:$JOBID $SCRIPT | tee tmp.txt
 	fi
         JOBID=$(cat tmp.txt)
 	echo ""
@@ -72,4 +79,5 @@ else
     
 fi
 
+history -w .input_history
 rm -f tmp.txt
