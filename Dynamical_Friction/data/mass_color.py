01424bd26f6ab6e5ea54e00937f9970f5261a440
kazpc1215 <isoya.kazuhide@d.mbox.nagoya-u.ac.jp>
2018-06-24 02:37:50 +0900

    plot.pyとmass_color.pyを変更．連番画像から動画を作った．

--- Dynamical_Friction/data/mass_color.py
+++ Dynamical_Friction/data/mass_color.py
@@ -11,97 +11,127 @@ def delta_axis(ratio):
 
 
 ######################################################################
-# directory = "Ntr1E3_t1E3yr_dt1yr_Mtot3E-5_ecc1E-2_inc5E-3_frag_rand1//"
-directory = "test/"
+# path = "/Users/isoya.kazuhide/Dynamical_Friction/data/"
+directory = "Ntr3E3_t1E3_dtlog_Mtot3E-5_ecc3E-2_frag_noacc/"
 
-
-Time = 8
 
 N_p = 3
 N_tr = 3000
 
-time = np.empty([N_p+N_tr, Time], dtype=float)  # (ファイル番号,行数)
-num = np.empty([N_p+N_tr, Time], dtype=float)
-x = np.empty([N_p+N_tr, Time], dtype=float)
-y = np.empty([N_p+N_tr, Time], dtype=float)
-z = np.empty([N_p+N_tr, Time], dtype=float)
-r_3d = np.empty([N_p+N_tr, Time], dtype=float)
-r_2d = np.empty([N_p+N_tr, Time], dtype=float)
-mass = np.empty([N_p+N_tr, Time], dtype=float)
-sigma = np.empty([N_p+N_tr, Time], dtype=float)
 
-# radius = np.empty([N_p+N_tr], dtype=float)
-# theta = np.empty([N_p+N_tr], dtype=float)
+if(N_p == 1):
+    LINE = 42  # 10000yr
+    SUBDIR_NUM = 40
+elif(N_p == 3):
+    LINE = 34  # 1000yr
+    SUBDIR_NUM = 13
 
-for T in range(Time):
-    arr = np.genfromtxt(directory + "Posi_Mass_%02d.dat" % T, dtype=np.float, delimiter="\t")
 
-    time[:, T] = arr[:, 0]
-    num[:, T] = arr[:, 1]
-    x[:, T] = arr[:, 2]
-    y[:, T] = arr[:, 3]
-    z[:, T] = arr[:, 4]
-    r_3d[:, T] = arr[:, 5]
-    r_2d[:, T] = arr[:, 6]
-    mass[:, T] = arr[:, 7]
-    sigma[:, T] = arr[:, 10]
-    print(T, int(num[3002, T]), time[3002, T], x[3002, T])
+for subnum in range(1, SUBDIR_NUM+1):
+    subdirctory = "rand%02d/" % subnum
 
-    fig = plt.figure(figsize=(8, 6), dpi=100)
+    time = np.empty([N_p+N_tr, LINE], dtype=float)  # (ファイル番号,行数)
+    num = np.empty([N_p+N_tr, LINE], dtype=float)
+    x = np.empty([N_p+N_tr, LINE], dtype=float)
+    y = np.empty([N_p+N_tr, LINE], dtype=float)
+    z = np.empty([N_p+N_tr, LINE], dtype=float)
+    r_3d = np.empty([N_p+N_tr, LINE], dtype=float)
+    r_2d = np.empty([N_p+N_tr, LINE], dtype=float)
+    mass = np.empty([N_p+N_tr, LINE], dtype=float)
+    sigma = np.empty([N_p+N_tr, LINE], dtype=float)
 
-    """
-    ax = fig.add_subplot(1, 1, 1, aspect='equal')
-    ax.set_xlim([-2, 2])
-    ax.set_ylim([-2, 2])
-    ax.set_xlabel('x [AU]', fontsize=20)
-    ax.set_ylabel('y [AU]', fontsize=20)
-    """
+    # radius = np.empty([N_p+N_tr], dtype=float)
+    # theta = np.empty([N_p+N_tr], dtype=float)
 
-    ax = fig.add_subplot(111, projection="polar")
-    # ax = fig.add_subplot(111)
-    ax.title.set_text('%.3f [yr]' % time[1, T])
-    ax.title.set_fontsize(15)
+    for T in range(LINE):
+        arr = np.genfromtxt(directory + subdirctory + "Posi_Mass_%02d.dat" % T, dtype=np.float, delimiter="\t")
+        if(N_p+N_tr - arr.shape[0] > 0):
+            print(T, "padding")
+            arr = np.pad(arr, [(0, N_p+N_tr - arr.shape[0]), (0, 0)], 'constant', constant_values=0.0)
 
-    fig.subplots_adjust(right=0.8)
+        time[:, T] = arr[:, 0]
+        num[:, T] = arr[:, 1]
+        x[:, T] = arr[:, 2]
+        y[:, T] = arr[:, 3]
+        z[:, T] = arr[:, 4]
+        r_3d[:, T] = arr[:, 5]
+        r_2d[:, T] = arr[:, 6]
+        mass[:, T] = arr[:, 7]
+        sigma[:, T] = arr[:, 10]
+        print(T, int(num[0, T]), time[0, T], x[0, T])
 
-    # cm = plt.cm.get_cmap('rainbow')
+        fig = plt.figure(figsize=(8, 6), dpi=100)
 
-    RADIUS = np.linspace(0.0, 1.5, 500)
-    THETA = np.linspace(-np.pi, np.pi, 500)
+        """
+        ax = fig.add_subplot(1, 1, 1, aspect='equal')
+        ax.set_xlim([-2, 2])
+        ax.set_ylim([-2, 2])
+        ax.set_xlabel('x [AU]', fontsize=20)
+        ax.set_ylabel('y [AU]', fontsize=20)
+        """
 
-    radius = np.sqrt(x[:, T]**2 + y[:, T]**2)
-    radius = np.append(radius, np.random.uniform(0.0, 1.0/delta_axis(10.0)/delta_axis(5.0), 10000))
-    radius = np.append(radius, np.random.uniform(delta_axis(10.0)*delta_axis(5.0), 1.5, 5000))
+        ax = fig.add_subplot(111, projection="polar")
+        # ax = fig.add_subplot(111)
+        ax.title.set_text('%.3e [yr]' % time[1, T])
+        ax.title.set_fontsize(15)
 
-    theta = np.arctan2(y[:, T], x[:, T])
-    theta = np.append(theta, np.random.uniform(-np.pi-0.1, np.pi+0.1, 15000))
+        fig.subplots_adjust(right=0.8)
 
-    sigma_grid = sigma[:, T]*8888888.88888889
-    sigma_grid = np.append(sigma_grid, [0.0 for i in range(15000)])
+        # cm = plt.cm.get_cmap('rainbow')
 
-    RADIUS, THETA = np.meshgrid(RADIUS, THETA)
-    Z = griddata((radius[3:], theta[3:]), sigma_grid[3:], (RADIUS, THETA), method="cubic")
-    Z_convert = np.where(Z < 0.0, 0.0, Z)
+        RADIUS = np.linspace(0.0, 1.5, 500)
+        THETA = np.linspace(-np.pi, np.pi, 500)
 
-    # im = ax.scatter(theta[3:], radius[3:], s=1, c=mass[3:, T] * 2E8, vmin=1.0, vmax=6.0, cmap=cm, label='Tracer')
+        radius = np.sqrt(x[:, T]**2 + y[:, T]**2)
 
-    # im = ax.contourf(THETA, RADIUS, Z, levels=np.linspace(0.0, 3.0, 100), cmap=cm)
-    # im = ax.contourf(THETA, RADIUS, Z_convert, cmap="jet")
-    im = ax.contourf(THETA, RADIUS, Z_convert, levels=np.linspace(0.0, 800.0, 100), cmap="jet")
-    # im = ax.contourf(THETA, RADIUS, Z_convert, norm=LogNorm(), cmap="jet")
+        if(N_p == 1):
+            radius = np.append(radius, np.random.uniform(0.0, 1.0/delta_axis(5.0), 10000))
+            radius = np.append(radius, np.random.uniform(delta_axis(5.0), 1.5, 5000))
+        elif(N_p == 3):
+            radius = np.append(radius, np.random.uniform(0.0, 1.0/delta_axis(10.0)/delta_axis(5.0), 10000))
+            radius = np.append(radius, np.random.uniform(delta_axis(10.0)*delta_axis(5.0), 1.5, 5000))
 
-    # ax.scatter(0, 0, c='k', marker='*', s=50, label='Star')
-    ax.scatter(theta[0], radius[0], c='k', marker='1', s=50, label='Planet1')
-    ax.scatter(theta[1], radius[1], c='k', marker='2', s=50, label='Planet2')
-    ax.scatter(theta[2], radius[2], c='k', marker='3', s=50, label='Planet3')
-    # ax.scatter(theta[3:], radius[3:], c='k', marker='.', s=1, label='tracer')
+        theta = np.arctan2(y[:, T], x[:, T])
+        theta = np.append(theta, np.random.uniform(-np.pi-0.1, np.pi+0.1, 15000))
 
-    ax.set_rticks([0.5, 1.0, 1.5])
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.03, 0.7])
-    fig.colorbar(im, cax=cbar_ax, ticks=np.linspace(0.0, 800.0, 9))
-    # cbar_ax.set_ylabel(r'mass $[10^{25} \rm g]$', fontsize=20)
-    cbar_ax.set_ylabel(r'$\Sigma \ \rm [g/cm^2]$', fontsize=20)
+        sigma_grid = sigma[:, T]*8888888.88888889
+        sigma_grid = np.append(sigma_grid, [0.0 for i in range(15000)])
 
-    # plt.tight_layout()
-    ax.legend()
-    plt.show()
+        RADIUS, THETA = np.meshgrid(RADIUS, THETA)
+
+        Z = griddata((radius[N_p:], theta[N_p:]), sigma_grid[N_p:], (RADIUS, THETA), method="linear")
+        Z[np.isnan(Z)] = 0.0
+        Z_convert = np.where(Z < 0.0, 0.0, Z)
+
+        # im = ax.scatter(theta[3:], radius[3:], s=1, c=mass[3:, T] * 2E8, vmin=1.0, vmax=6.0, cmap=cm, label='Tracer')
+
+        # im = ax.contourf(THETA, RADIUS, Z, levels=np.linspace(0.0, 3.0, 100), cmap=cm)
+        # im = ax.contourf(THETA, RADIUS, Z_convert, cmap="jet")
+        im = ax.contourf(THETA, RADIUS, Z_convert, levels=np.linspace(0.0, np.amax(Z), 100), cmap="jet")
+        # im = ax.contourf(THETA, RADIUS, Z_convert, norm=LogNorm(), levels=[1E-1, 1E0, 1E1, 1E2, 1E3], cmap="jet")
+
+        # ax.scatter(0, 0, c='k', marker='*', s=50, label='Star')
+        if(N_p == 1):
+            ax.scatter(theta[0], radius[0], c='k', marker='1', s=50, label='Planet')
+        elif(N_p == 3):
+            ax.scatter(theta[0], radius[0], c='k', marker='1', s=50, label='Planet1')
+            ax.scatter(theta[1], radius[1], c='k', marker='2', s=50, label='Planet2')
+            ax.scatter(theta[2], radius[2], c='k', marker='3', s=50, label='Planet3')
+        # ax.scatter(theta[3:], radius[3:], c='k', marker='.', s=1, label='tracer')
+
+        ax.set_rlim(0, 1.5)
+        ax.set_rticks([0.5, 1.0, 1.5])
+        ax.set_thetalim(0, 2*np.pi)
+        ax.set_xticks(np.pi/180. * np.linspace(0, 360, 4, endpoint=False))
+        cbar_ax = fig.add_axes([0.85, 0.15, 0.03, 0.7])
+        fig.colorbar(im, cax=cbar_ax, ticks=np.linspace(0.0, np.amax(Z), 10))
+        # fig.colorbar(im, cax=cbar_ax)
+        # cbar_ax.set_ylabel(r'mass $[10^{25} \rm g]$', fontsize=20)
+        cbar_ax.set_ylabel(r'$\Sigma \ \rm [g/cm^2]$', fontsize=20)
+
+        # plt.tight_layout()
+        ax.legend()
+        filename = "../image/" + directory + subdirctory + "Sigma_T%02d.png" % T
+        plt.savefig(filename, format="png", dpi=100)
+        # plt.show()
+        plt.close()
--- Dynamical_Friction/data/plot.py
+++ Dynamical_Friction/data/plot.py
@@ -13,13 +13,20 @@ def Jacobi(axis):
 
 
 ######################################################################
-# path = "/Users/isoya.kazuhide/Dynamical_Friction/data//"
-directory = "Ntr3E3_t1E3yr_dt1yr_Mtot3E-5_ecc1E-2_inc5E-3_frag_rand4//"
+# path = "/Users/isoya.kazuhide/Dynamical_Friction/data/"
+directory = "Ntr1E3_t1E4_dtlog_Mtot3E-5_ecc3E-2_nofrag_noacc/"
 
-LINE = 1001
 
-N_p = 3
-N_tr = 3000
+N_p = 1
+N_tr = 1000
+
+
+if(N_p == 1):
+    LINE = 42  # 10000yr
+    SUBDIR_NUM = 40
+elif(N_p == 3):
+    LINE = 34  # 1000yr
+    SUBDIR_NUM = 13
 
 X_MESH = 2000
 Y_MESH = 5000
@@ -29,106 +36,117 @@ y = [i/Y_MESH for i in range(int(0.2*Y_MESH)+1)]
 x, y = np.meshgrid(x, y)
 
 
-time = np.empty([N_p+N_tr+1, LINE], dtype=float)  # (ファイル番号,行数)
-ecc = np.empty([N_p+N_tr+1, LINE], dtype=float)
-axis = np.empty([N_p+N_tr+1, LINE], dtype=float)
-u = np.empty([N_p+N_tr+1, LINE], dtype=float)
-inc = np.empty([N_p+N_tr+1, LINE], dtype=float)
-Omega = np.empty([N_p+N_tr+1, LINE], dtype=float)
-omega = np.empty([N_p+N_tr+1, LINE], dtype=float)
-r_h = np.empty([N_p+N_tr+1, LINE], dtype=float)
-radius = np.empty([N_p+N_tr+1, LINE], dtype=float)
-mass = np.empty([N_p+N_tr+1, LINE], dtype=float)
-#####
-for n in range(1, N_p+1):
-    arr = np.genfromtxt(directory + "Planet%02d.dat" % n, dtype=np.float, delimiter="\t")
+for subnum in range(SUBDIR_NUM, SUBDIR_NUM+1):
+    subdirctory = "rand%02d/" % subnum
 
-    time[n, :] = arr[:, 0]
-    ecc[n, :] = arr[:, 1]
-    axis[n, :] = arr[:, 2]
-    # u[n, :] = arr[:, 3]
-    inc[n, :] = arr[:, 4]
-    # Omega[n, :] = arr[:, 5]
-    # omega[n, :] = arr[:, 6]
-    # r_h[n, :] = arr[:, 7]
-    # radius[n, :] = arr[:, 8]
-    # mass[n, :] = arr[:, 9]
-    print(n, time[n, 1], axis[n, 1], ecc[n, 1], inc[n, 1])
-
-#####
-for n in range(N_p+1, N_p+N_tr+1):
-    arr = np.genfromtxt(directory + "tracer%06d.dat" % (n-N_p), dtype=np.float, delimiter="\t")
-
-    time[n, :] = arr[:, 0]
-    ecc[n, :] = arr[:, 1]
-    axis[n, :] = arr[:, 2]
-    # u[n, :] = arr[:, 3]
-    inc[n, :] = arr[:, 4]
-    # Omega[n, :] = arr[:, 5]
-    # omega[n, :] = arr[:, 6]
-    # r_h[n, :] = arr[:, 7]
-    # radius[n, :] = arr[:, 8]
-    # mass[n, :] = arr[:, 9]
-    print(n, time[n, 1], axis[n, 1], ecc[n, 1], inc[n, 1])
-
-#####
-
-for T in range(0, LINE):
+    time = np.empty([N_p+N_tr+1, LINE], dtype=float)  # (ファイル番号,行数)
+    ecc = np.empty([N_p+N_tr+1, LINE], dtype=float)
+    axis = np.empty([N_p+N_tr+1, LINE], dtype=float)
+    u = np.empty([N_p+N_tr+1, LINE], dtype=float)
+    inc = np.empty([N_p+N_tr+1, LINE], dtype=float)
+    Omega = np.empty([N_p+N_tr+1, LINE], dtype=float)
+    omega = np.empty([N_p+N_tr+1, LINE], dtype=float)
+    r_h = np.empty([N_p+N_tr+1, LINE], dtype=float)
+    radius = np.empty([N_p+N_tr+1, LINE], dtype=float)
+    mass = np.empty([N_p+N_tr+1, LINE], dtype=float)
     #####
-    plt.figure(figsize=(10, 8), dpi=100)
-    if(N_p == 1):
-        plt.title(r"$N_{\rm tr}=1000,M_{\rm tot}=10 {\rm M_{\oplus}},{\rm time}: %05.0f {\rm yr}$" % time[1, T], fontsize=18)
-        plt.xlim([0.8, 1.2])
-    elif(N_p == 3):
-        plt.title(r"$N_{\rm tr}=3000,M_{\rm tot}=30 {\rm M_{\oplus}},{\rm time}: %05.0f {\rm yr}$" % time[1, T], fontsize=18)
-        plt.xlim([0.75, 1.3])
+    for n in range(1, N_p+1):
+        arr = np.genfromtxt(directory + subdirctory + "Planet%02d.dat" % n, dtype=np.float, delimiter="\t")
+        if(LINE - arr.shape[0] > 0):
+            print(n, "padding")
+            arr = np.pad(arr, [(0, LINE - arr.shape[0]), (0, 0)], 'constant', constant_values=np.nan)
+            # print(arr)
 
-    # plt.ylim([0, 0.35])
-    plt.ylim([0, 0.2])
-    plt.xlabel('semi-major axis [AU]', fontsize=25)
-    plt.ylabel('ecc', fontsize=25)
-
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.grid(True)
-
-    plt.contour(x, y, Jacobi(axis[1, T]), colors=["g"], levels=[3.0], linestyles="dashed", linewidths=0.5)
-    if(N_p == 3):
-        plt.contour(x, y, Jacobi(axis[2, T]), colors=["g"], levels=[3.0], linestyles="dashed", linewidths=0.5)
-        plt.contour(x, y, Jacobi(axis[3, T]), colors=["g"], levels=[3.0], linestyles="dashed", linewidths=0.5)
-    plt.scatter(axis[N_p+1:, T], ecc[N_p+1:, T], color="b", s=5, label="Tracer")
-    plt.scatter(axis[1:N_p+1, T], ecc[1:N_p+1, T], color="r", s=20, label="Planet")
-    plt.legend(loc="upper left", fontsize=15)
-    plt.tight_layout()
-    filename = "../image/" + directory + "axis_ecc_T%05.0fyr.png" % time[1, T]
-    plt.savefig(filename)
-    plt.close()
-    #####
+        time[n, :] = arr[:, 0]
+        ecc[n, :] = arr[:, 1]
+        axis[n, :] = arr[:, 2]
+        # u[n, :] = arr[:, 3]
+        inc[n, :] = arr[:, 4]
+        # Omega[n, :] = arr[:, 5]
+        # omega[n, :] = arr[:, 6]
+        # r_h[n, :] = arr[:, 7]
+        # radius[n, :] = arr[:, 8]
+        # mass[n, :] = arr[:, 9]
+        print(n, time[n, 1], axis[n, 1], ecc[n, 1], inc[n, 1])
 
     #####
-    plt.figure(figsize=(10, 8), dpi=100)
-    if(N_p == 1):
-        plt.title(r"$N_{\rm tr}=1000,M_{\rm tot}=10 {\rm M_{\oplus}},{\rm time}: %05.0f {\rm yr}$" % time[1, T], fontsize=18)
-        plt.xlim([0.8, 1.2])
-    elif(N_p == 3):
-        plt.title(r"$N_{\rm tr}=3000,M_{\rm tot}=30 {\rm M_{\oplus}},{\rm time}: %05.0f {\rm yr}$" % time[1, T], fontsize=18)
-        plt.xlim([0.75, 1.3])
+    for n in range(N_p+1, N_p+N_tr+1):
+        arr = np.genfromtxt(directory + subdirctory + "tracer%06d.dat" % (n-N_p), dtype=np.float, delimiter="\t")
+        if(LINE - arr.shape[0] > 0):
+            print(n, "padding")
+            arr = np.pad(arr, [(0, LINE - arr.shape[0]), (0, 0)], 'constant', constant_values=np.nan)
+            # print(arr)
 
-    # plt.ylim([0, 0.16])
-    plt.ylim([0, 0.1])
-    plt.xlabel('semi-major axis [AU]', fontsize=25)
-    plt.ylabel('inc [rad]', fontsize=25)
+        time[n, :] = arr[:, 0]
+        ecc[n, :] = arr[:, 1]
+        axis[n, :] = arr[:, 2]
+        # u[n, :] = arr[:, 3]
+        inc[n, :] = arr[:, 4]
+        # Omega[n, :] = arr[:, 5]
+        # omega[n, :] = arr[:, 6]
+        # r_h[n, :] = arr[:, 7]
+        # radius[n, :] = arr[:, 8]
+        # mass[n, :] = arr[:, 9]
+        print(n, time[n, 1], axis[n, 1], ecc[n, 1], inc[n, 1])
 
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.grid(True)
-    plt.scatter(axis[N_p+1:, T], inc[N_p+1:, T], color="b", s=5, label="Tracer")
-    plt.scatter(axis[1:N_p+1, T], inc[1:N_p+1, T], color="r", s=20, label="Planet")
-    plt.legend(loc="upper left", fontsize=15)
-    plt.tight_layout()
-    filename = "../image/" + directory + "axis_inc_T%05.0fyr.png" % time[1, T]
-    plt.savefig(filename)
-    plt.close()
     #####
-    print(time[1, T])
-    # plt.show()
+
+    for T in range(0, LINE):
+        #####
+        plt.figure(figsize=(10, 8), dpi=100)
+        if(N_p == 1):
+            plt.title(r"$N_{\rm tr}=1000,M_{\rm tot}=10 {\rm M_{\oplus}},{\rm time}: %.3e {\rm yr}$" % time[1, T], fontsize=18)
+            plt.xlim([0.8, 1.2])
+        elif(N_p == 3):
+            plt.title(r"$N_{\rm tr}=3000,M_{\rm tot}=30 {\rm M_{\oplus}},{\rm time}: %.3e {\rm yr}$" % time[1, T], fontsize=18)
+            plt.xlim([0.75, 1.3])
+
+        # plt.ylim([0, 0.35])
+        plt.ylim([0, 0.2])
+        plt.xlabel('semi-major axis [AU]', fontsize=25)
+        plt.ylabel('ecc', fontsize=25)
+
+        plt.xticks(fontsize=15)
+        plt.yticks(fontsize=15)
+        plt.grid(True)
+
+        plt.contour(x, y, Jacobi(axis[1, T]), colors=["g"], levels=[3.0], linestyles="dashed", linewidths=0.5)
+        if(N_p == 3):
+            plt.contour(x, y, Jacobi(axis[2, T]), colors=["g"], levels=[3.0], linestyles="dashed", linewidths=0.5)
+            plt.contour(x, y, Jacobi(axis[3, T]), colors=["g"], levels=[3.0], linestyles="dashed", linewidths=0.5)
+        plt.scatter(axis[N_p+1:, T], ecc[N_p+1:, T], color="b", s=5, label="Tracer")
+        plt.scatter(axis[1:N_p+1, T], ecc[1:N_p+1, T], color="r", s=20, label="Planet")
+        plt.legend(loc="upper left", fontsize=15)
+        plt.tight_layout()
+        filename = "../image/" + directory + subdirctory + "axis_ecc_T%02d.png" % T
+        plt.savefig(filename)
+        plt.close()
+        #####
+
+        #####
+        plt.figure(figsize=(10, 8), dpi=100)
+        if(N_p == 1):
+            plt.title(r"$N_{\rm tr}=1000,M_{\rm tot}=10 {\rm M_{\oplus}},{\rm time}: %.3e {\rm yr}$" % time[1, T], fontsize=18)
+            plt.xlim([0.8, 1.2])
+        elif(N_p == 3):
+            plt.title(r"$N_{\rm tr}=3000,M_{\rm tot}=30 {\rm M_{\oplus}},{\rm time}: %.3e {\rm yr}$" % time[1, T], fontsize=18)
+            plt.xlim([0.75, 1.3])
+
+        # plt.ylim([0, 0.16])
+        plt.ylim([0, 0.1])
+        plt.xlabel('semi-major axis [AU]', fontsize=25)
+        plt.ylabel('inc [rad]', fontsize=25)
+
+        plt.xticks(fontsize=15)
+        plt.yticks(fontsize=15)
+        plt.grid(True)
+        plt.scatter(axis[N_p+1:, T], inc[N_p+1:, T], color="b", s=5, label="Tracer")
+        plt.scatter(axis[1:N_p+1, T], inc[1:N_p+1, T], color="r", s=20, label="Planet")
+        plt.legend(loc="upper left", fontsize=15)
+        plt.tight_layout()
+        filename = "../image/" + directory + subdirctory + "axis_inc_T%02d.png" % T
+        plt.savefig(filename)
+        plt.close()
+        #####
+        print(time[1, T])
+        # plt.show()
