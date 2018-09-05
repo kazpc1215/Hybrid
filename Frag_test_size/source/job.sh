1fc9ed77d28cfbea3def00e6f2d3f68881744976
kazpc1215 <isoya.kazuhide@d.mbox.nagoya-u.ac.jp>
2018-09-06 02:41:17 +0900

    ecc=0.05のデータ

--- Frag_test_size/source/job.sh
+++ Frag_test_size/source/job.sh
@@ -4,4 +4,4 @@
 cd $PBS_O_WORKDIR
 export OMP_NUM_THREADS=1
 export CRAY_OMP_CHECK_AFFINITY=TRUE
-aprun -n 1 -d $OMP_NUM_THREADS ./Nc1E3_t1E2_dtlog_Mtot3E-5_Mmax5E-15_ecc1E-2_Rayleigh_frag_dr4E-2_dtheta0.125pi.exe >> /work/isoyakz/Dynamical_Friction/log/Nc1E3_t1E2_dtlog_Mtot3E-5_Mmax5E-15_ecc1E-2_Rayleigh_frag_dr4E-2_dtheta0.125pi.txt
+aprun -n 1 -d $OMP_NUM_THREADS ./Nc1E3_t1E2_dtlog_Mtot3E-5_Mmax5E-15_ecc3E-2_adjust_frag_dr2E-3_dtheta0.125pi.exe >> /work/isoyakz/Dynamical_Friction/log/Nc1E3_t1E2_dtlog_Mtot3E-5_Mmax5E-15_ecc3E-2_adjust_frag_dr2E-3_dtheta0.125pi.txt
