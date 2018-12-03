48cfc17bc052157c8aeabb3224ee7c2e4b04911a
kazpc1215 <isoya.kazuhide@d.mbox.nagoya-u.ac.jp>
2018-12-03 18:20:54 +0900

    エネルギー誤差などの画像

--- Dynamical_Friction/data/collision.py
+++ Dynamical_Friction/data/collision.py
@@ -27,9 +27,9 @@ for subnum in range(1, SUBDIR_NUM+1):
     mass_tr = 3.0E-9
     total_mass = 1.0 + 3.0E-6 + 3.0E-7  # 中心星込みの総質量
     dE_correct = 0.0
-    dL_x = 0.0
-    dL_y = 0.0
-    dL_z = 0.0
+    dLx = 0.0
+    dLy = 0.0
+    dLz = 0.0
 
 
     """
@@ -39,8 +39,34 @@ for subnum in range(1, SUBDIR_NUM+1):
     """
 
     with open(directory + subdirectory + "Collision_time.dat", "w") as f:
-        print("# time[yr]\tnumber", file=f)
-        print(0.0, "\t", N_tr, file=f, sep="")
+        print("#1:time",
+              "2:N_tr",
+              "3:total_energy",
+              "4:rel_total_energy",
+              "5:dE_correct",
+              "6:rel_dE_correct",
+              "7:energy_p",
+              "8:rel_energy_p",
+              "9:total_angmom.",
+              "10:rel_total_angmom.",
+              "11:abs_L_p",
+              "12:rel_abs_L_p",
+              "13:total_Lx",
+              "14:rel_total_Lx",
+              "15:dLx",
+              "16:rel_dLx",
+              "17:total_Ly",
+              "18:rel_total_Ly",
+              "19:dLy",
+              "20:rel_dLy",
+              "21:total_Lz",
+              "22:rel_total_Lz",
+              "23:dLz",
+              "24:rel_dLz",
+              file=f, sep="\t")
+
+        print(0.0, N_tr, file=f, sep="\t")
+
 
     for n_col in range(1, N_tr+1):
 
@@ -104,24 +130,24 @@ for subnum in range(1, SUBDIR_NUM+1):
             temp = np.zeros([N_p+N_tr-n_col], dtype=float)
 
             ##########
+            energy_p = 0.5 * mass_p * ((vx[subnum-1, 0] - vx_G)**2 + (vy[subnum-1, 0] - vy_G)**2 + (vz[subnum-1, 0] - vz_G)**2) - mass_p / abs_r[subnum-1, 0]
             energy_i = 0.5 * mass_tr * ((vx[subnum-1, 1:] - vx_G)**2 + (vy[subnum-1, 1:] - vy_G)**2 + (vz[subnum-1, 1:] - vz_G)**2)  # indirect term 有りの tracer の運動エネルギー
             energy_i += - mass_tr / abs_r[subnum-1, 1:]  # planet（0）を除く、中心星からの重力エネルギー
             energy_rel = - mass_p * mass_tr / np.sqrt((x[subnum-1, 1:] - x[subnum-1, 0])**2 + (y[subnum-1, 1:] - y[subnum-1, 0])**2 + (z[subnum-1, 1:] - z[subnum-1, 0])**2)  # tracer と planet（0）との相互重力エネルギー
-            energy_p = 0.5 * mass_p * ((vx[subnum-1, 0] - vx_G)**2 + (vy[subnum-1, 0] - vy_G)**2 + (vz[subnum-1, 0] - vz_G)**2) - mass_p / abs_r[subnum-1, 0]
             total_energy = 0.5 * (vx_G**2 + vy_G**2 + vz_G**2) + energy_p + energy_i.sum() + energy_rel.sum() + dE_correct
             ##########
 
 
             ##########
-            Lx_p = mass_p * (y[subnum-1, 0] * vz[subnum-1, 0] - z[subnum-1, 0] * vy[subnum-1, 0]) - dL_x
-            Ly_p = mass_p * (z[subnum-1, 0] * vx[subnum-1, 0] - x[subnum-1, 0] * vz[subnum-1, 0]) - dL_y
-            Lz_p = mass_p * (x[subnum-1, 0] * vy[subnum-1, 0] - y[subnum-1, 0] * vx[subnum-1, 0]) - dL_z
+            Lx_p = mass_p * (y[subnum-1, 0] * vz[subnum-1, 0] - z[subnum-1, 0] * vy[subnum-1, 0])
+            Ly_p = mass_p * (z[subnum-1, 0] * vx[subnum-1, 0] - x[subnum-1, 0] * vz[subnum-1, 0])
+            Lz_p = mass_p * (x[subnum-1, 0] * vy[subnum-1, 0] - y[subnum-1, 0] * vx[subnum-1, 0])
             Lx_i = mass_tr * (y[subnum-1, 1:] * vz[subnum-1, 1:] - z[subnum-1, 1:] * vy[subnum-1, 1:])
             Ly_i = mass_tr * (z[subnum-1, 1:] * vx[subnum-1, 1:] - x[subnum-1, 1:] * vz[subnum-1, 1:])
             Lz_i = mass_tr * (x[subnum-1, 1:] * vy[subnum-1, 1:] - y[subnum-1, 1:] * vx[subnum-1, 1:])
-            total_Lx = Lx_p + Lx_i.sum()
-            total_Ly = Ly_p + Ly_i.sum()
-            total_Lz = Lz_p + Lz_i.sum()
+            total_Lx = Lx_p + Lx_i.sum() - dLx
+            total_Ly = Ly_p + Ly_i.sum() - dLy
+            total_Lz = Lz_p + Lz_i.sum() - dLz
             abs_L_p = np.sqrt(Lx_p**2 + Ly_p**2 + Lz_p**2)
             total_angularmomentum = np.sqrt(total_Lx**2 + total_Ly**2 + total_Lz**2)
             ##########
@@ -172,16 +198,40 @@ for subnum in range(1, SUBDIR_NUM+1):
             vy_g12 = (mass_p * vy[subnum-1, 0] + mass_tr * vy[subnum-1, j_col-1]) / (mass_p + mass_tr)
             vz_g12 = (mass_p * vz[subnum-1, 0] + mass_tr * vz[subnum-1, j_col-1]) / (mass_p + mass_tr)
 
-            dL_x += (mass_p + mass_tr) * (y_g12 * vz_g12 - z_g12 * vy_g12) - mass_p * (y[subnum-1, 0] * vz[subnum-1, 0] - z[subnum-1, 0] * vy[subnum-1, 0]) - mass_tr * (y[subnum-1, j_col-1] * vz[subnum-1, j_col-1] - z[subnum-1, j_col-1] * vy[subnum-1, j_col-1])
-            dL_y += (mass_p + mass_tr) * (z_g12 * vx_g12 - x_g12 * vz_g12) - mass_p * (z[subnum-1, 0] * vx[subnum-1, 0] - x[subnum-1, 0] * vz[subnum-1, 0]) - mass_tr * (z[subnum-1, j_col-1] * vx[subnum-1, j_col-1] - x[subnum-1, j_col-1] * vz[subnum-1, j_col-1])
-            dL_z += (mass_p + mass_tr) * (x_g12 * vy_g12 - y_g12 * vx_g12) - mass_p * (x[subnum-1, 0] * vy[subnum-1, 0] - y[subnum-1, 0] * vx[subnum-1, 0]) - mass_tr * (x[subnum-1, j_col-1] * vy[subnum-1, j_col-1] - y[subnum-1, j_col-1] * vx[subnum-1, j_col-1])
-            print(n_col, dL_x, dL_y, dL_z)
+            dLx += (mass_p + mass_tr) * (y_g12 * vz_g12 - z_g12 * vy_g12) - mass_p * (y[subnum-1, 0] * vz[subnum-1, 0] - z[subnum-1, 0] * vy[subnum-1, 0]) - mass_tr * (y[subnum-1, j_col-1] * vz[subnum-1, j_col-1] - z[subnum-1, j_col-1] * vy[subnum-1, j_col-1])
+            dLy += (mass_p + mass_tr) * (z_g12 * vx_g12 - x_g12 * vz_g12) - mass_p * (z[subnum-1, 0] * vx[subnum-1, 0] - x[subnum-1, 0] * vz[subnum-1, 0]) - mass_tr * (z[subnum-1, j_col-1] * vx[subnum-1, j_col-1] - x[subnum-1, j_col-1] * vz[subnum-1, j_col-1])
+            dLz += (mass_p + mass_tr) * (x_g12 * vy_g12 - y_g12 * vx_g12) - mass_p * (x[subnum-1, 0] * vy[subnum-1, 0] - y[subnum-1, 0] * vx[subnum-1, 0]) - mass_tr * (x[subnum-1, j_col-1] * vy[subnum-1, j_col-1] - y[subnum-1, j_col-1] * vx[subnum-1, j_col-1])
+            print(n_col, dLx, dLy, dLz)
             print("")
             ##########
 
 
             with open(directory + subdirectory + "Collision_time.dat", "a") as f:
-                print(time[subnum-1, 0], "\t", N_tr - n_col, "\t", abs((total_energy - total_energy_0)/total_energy_0), "\t", dE_correct / total_energy_0, file=f, sep="")
+                print(time[subnum-1, 0],
+                      N_tr - n_col,
+                      total_energy,
+                      (total_energy - total_energy_0) / abs(total_energy_0),
+                      dE_correct,
+                      dE_correct / abs(total_energy_0),
+                      energy_p,
+                      (energy_p - energy_p_0) / abs(energy_p_0),
+                      total_angularmomentum,
+                      (total_angularmomentum - total_angularmomentum_0) / abs(total_angularmomentum_0),
+                      abs_L_p,
+                      (abs_L_p - abs_L_p_0) / abs(abs_L_p_0),
+                      total_Lx,
+                      (total_Lx - total_Lx_0) / abs(total_Lx_0),
+                      dLx,
+                      dLx / abs(total_Lx_0),
+                      total_Ly,
+                      (total_Ly - total_Ly_0) / abs(total_Ly_0),
+                      dLy,
+                      dLy / abs(total_Ly_0),
+                      total_Lz,
+                      (total_Lz - total_Lz_0) / abs(total_Lz_0),
+                      dLz,
+                      dLz / abs(total_Lz_0),
+                      file=f, sep="\t")
 
 
             mass_p += mass_tr
